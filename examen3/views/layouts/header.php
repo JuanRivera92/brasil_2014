90263edaf7bcb1d874979bbb6c20eaa163fba983
Juan Rivera <rivera_r_m92@hotmail.com>
2014-07-15 15:03:45 -0500

    Examen3

---
+++ examen3/views/layouts/header.php
@@ -0,0 +1,87 @@
+<?php 
+  define('BASEURL','http://localhost:8080/brasil_2014/proyecto1/');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/bootstrap.js"></script>	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Clase programación web verano</title>
+	
+	<!-- Librerias -->
+	
+			<!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.css"/>
+    <link rel="stylesheet" href="../css/bootstrapValidator.min.css"/>
+	<link href="../css/micss.css" rel="stylesheet">	
+			
+			<!-- Fancybox -->
+	<link rel="stylesheet" type="text/css" href="../css/jquery.fancybox.css?v=2.1.5" media="screen" />
+	<link rel="stylesheet" type="text/css" href="../css/jquery.fancybox-buttons.css?v=1.0.5" />
+	<link rel="stylesheet" type="text/css" href="../css/jquery.fancybox-thumbs.css?v=1.0.7" />
+
+	<!-- Fin de Librerias -->
+
+	
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+		
+  </head>
+
+  <body role="document">
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Programación Web</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo BASEURL; ?>/views/site/inicio2.php">Inicio</a></li>
+            <li><a href="<?php echo BASEURL; ?>/views/ciudad/ciudad2.php">Ciudad</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/continente/continente2.php">Continente</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/estadio/estadio2.php">Estadio</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/pais/pais2.php">Pais</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/posicion/posicion2.php">Posicion</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/equipo/equipo2.php">Equipo</a></li>
+			<li><a href="<?php echo BASEURL; ?>/views/integrante/integrante2.php">Integrante</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">opciones  <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="">op1</a></li>
+                <li class="divider"></li>
+                <li><a href="">op2</a></li>
+              </ul>
+            </li>
+
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="<?php echo BASEURL; ?>/views/site/login.php">
+              <span class="glyphicon glyphicon-log-in"></span> Login</a>
+            </li>
+          </ul>
+          
+
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container theme-showcase" role="main">
+
---
+++ examen3/views/site/inicio2.php
@@ -0,0 +1,270 @@
+<?php 
+  session_start();
+  include ('../layouts/header.php');
+?>
+		<!-- Primer Renglon -->
+			<div class="row">
+				<div class="col-md-2">
+				<p class="text-right"> 
+				<img src="images\2014fwcoe_4cf_ps.png" class="img-rounded">
+				</p>
+				</div>
+				<div class="col-md-4">
+				<p class="text-right"> 
+				<img src="images\fifa2014rr.png" class="img-rounded">
+				</p>
+				</div>
+				<div class="col-md-6">
+				<p class="text-left">
+				<img width="" height="" src="images\organisation-megamenu-thumb.png" class="img-rounded">
+				</p>
+				</div>
+			</div>
+		<!-- Fin del Primer Renglon -->
+		<!-- Segundo Renglon -->
+			<div class="row" >
+				<div class="col-md-12" align?"center">
+					<ul class="nav nav-tabs navbar-right" role="tablist">
+					  <li><a href="#"><span class="glyphicon glyphicon-filter"></span>Noticias</a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-comment"></span>Fotos</a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-fire"></span>Videos</a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-flag"></span>Juegos</a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-headphones"></span>Organizacion</a></li>
+					</ul>
+				</div>
+			</div>
+
+		<!-- Fin del Segundo Renglon -->
+		
+		<!-- Tercer Renglon -->
+			<div class="row">
+				<div class="col-md-12">
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Partidos <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Equipos <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Jugadores <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Grupos <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Estadisticas <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Premios <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Entadas <span class="caret"></span></button>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Destino <span class="caret"></span></button>
+				</div>
+			</div>
+		<!-- Fin del Tercer Renglon -->
+		
+		<!-- Cuarto Renglon -->
+			<div class="row">
+				<h5>Siguenos en: 
+				<span class="glyphicon glyphicon-search"><a href="#"></a></span> 
+				<span class="glyphicon glyphicon-thumbs-up"></span> 
+				<span class="glyphicon glyphicon-globe"></span> 
+				<span class="glyphicon glyphicon-link"></span></h5> 
+			</div>		
+		<!-- Fin del Cuarto Renglon -->
+		
+		<!-- Quinto Renglon -->
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+			<h3>Martes 01 Julio</h3>
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+		<!-- Fin de Quinto Renglon -->
+		
+		<!-- Sexto Renglon -->
+		
+		<div class="row">
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-4">
+				<table  align="center">
+					<tr>
+						<th colspan="3" align="center"><h6>Final del partido</h6></th>
+					</tr>
+					<tr>
+						<th>Argentina</th>
+						<th>1 - 0</th>
+						<th>Suiza</th>
+					</tr>
+					<tr>
+						<th colspan="3" align="center"><h6>Argentina gana en la p&oacute;rra</h6></th>
+					</tr>
+					
+				</table>
+			</div>
+			<div class="col-md-4">
+				<table  align="center">
+					<tr>
+						<th colspan="3" align="center"><h6>Final del partido</h6></th>
+					</tr>
+					<tr>
+						<th>B&eacute;lgica</th>
+						<th>2 - 1</th>
+						<th>EEUU</th>
+					</tr>
+					<tr>
+						<th colspan="3" align="center"><h6>B&eacute;lgica gana en la p&oacute;rra</h6></th>
+					</tr>
+				</table>
+			</div>
+			<div class="col-md-2">
+			</div>
+		</div>
+		<!-- Fin de Sexto Renglon -->
+		
+		<!-- Septimo Renglon -->
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+			<img src="images\portero.jpg" class="img-rounded">
+			<h6>COSTA RICA</h6>
+			<a href="#"><h3>Cronica de un penal anunciado</h3><a/>
+			<h6>Miercoles 2 Julio 2014</h6>
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+		<br/>
+		<br/>
+		<!-- Fin de Septimo Renglon -->
+		
+		<!-- Octavo Renglon -->
+		
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-7">
+				<div class="row">
+					<div class="col-md-6">
+					<img src="images\2397535_xbig-lnd.jpg" class="img-rounded">
+					<h6>Estados Unidos</h6>
+					<h4><a href="#">Howard asombra al mundo</a></h4>
+					<h6>Miercoles 2 Julio 2014</h6>
+					</div>
+					<div class="col-md-6">
+					<img src="images\2390864_large-lnd.jpg" class="img-rounded">
+					<h6>COLOMBIA</h6>
+					<h4><a href="#">Veinte años sin Escobar</a></h4>
+					<h6>Miercoles 2 Julio 2014</h6>
+					</div>
+				</div>
+				
+				<br/>
+				<div class="row">
+					<div class="col-md-6">
+					<img src="images\3.jpg" class="img-rounded">
+					<h6>PAÍSES BAJOS</h6>
+					<h4><a href="#">Robben: "Me estoy divirtiendo mucho"</a></h4>
+					<h6>Miercoles 2 Julio 2014</h6>
+					</div>
+					<div class="col-md-6">
+					<img src="images\4.jpg" class="img-rounded">
+					<h6>BRAZIL 2014</h6>
+					<h4><a href="#">Partidos clásicos: Argentina-Bélgica en España 1982</a></h4>
+					<h6>Miercoles 2 Julio 2014</h6>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+			<img src="images\asd.png" class="img-rounded">
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+		<br/>
+		<br/>
+		
+		<!-- Fin de Octavo Renglon -->
+		
+		<div class="container" >
+			<div class="row" >
+				<div class="col-md-12" align?"center">
+					<ul class="nav nav-tabs navbar-right" role="tablist">
+					  <li><a href="#"><span class="glyphicon glyphicon-filter"></span>Todas las noticias ></a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-comment"></span>M&aacute;s Fotos ></a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-fire"></span>M&aacute;s Videos ></a></li>
+					  <li><a href="#"><span class="glyphicon glyphicon-flag"></span>Archivos del Blog ></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<br/>
+		<br/>
+		
+		<!-- Noveno Renglon -->
+		
+		<div class="row">
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-8">
+			<h4>Disfruta tu experiencia Brasil 2014</h4>
+			</div>
+			<div class="col-md-2">
+			</div>
+		</div>
+		
+		<!-- Fin de Noveno Renglon -->
+		
+		<!-- Decimo Renglon -->
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+				<div class="col-md-4">
+				<a href="#"><img src="images\Alojamiento.png" class="img-rounded"></a>
+				</div>
+				<div class="col-md-4">
+				<a href="#"><img src="images\Entradas.png" class="img-rounded"></a>
+				</div>
+				<div class="col-md-4">
+				<a href="#"><img src="images\Hospitalidad.png" class="img-rounded"></a>
+				</div>
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+		<br/>
+		<br/>
+		
+		<!-- Fin de Decimo Renglon -->
+		
+		<!-- 11 Renglon -->
+		
+		<div class="row">
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-8">
+			<h4>Torneo FIFA</h4>
+			</div>
+			<div class="col-md-2">
+			</div>
+		</div>
+		
+		<!-- Fin de 11 Renglon -->
+		
+		<!-- Octavo Renglon -->
+		
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+				<div class="col-md-4">
+				<img src="images\wtrscz.png" class="img-rounded">
+				</div>
+				<div class="col-md-4">
+				<img src="images\ewrtye.png" class="img-rounded">
+				</div>
+				<div class="col-md-4">
+				<img src="images\24536.png" class="img-rounded">
+				</div>
+			</div>
+			<div class="col-md-1">
+			</div>
+		</div>
+		
+		<!-- Fin de Octavo Renglon -->
+
+<?php include ('../layouts/footer.php'); ?>
